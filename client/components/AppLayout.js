272b5f5051d48ab36e80e04d6d0d514fc0090b0a
OH-JOO-YEONG <brb1111@naver.com>
2021-03-04 21:19:15 +0900

    Chore: eslint 패키지 설치 및 설정 Dev 패키지 모듈 추가 'eslint': '^7.21.0', 'eslint-plugin-import: ^2.22.1', 'eslint-plugin-react: ^7.22.0', 'eslint-plugin-react-hooks': '^4.2.0' eslintrc 설정
    Resolves #1

--- client/components/AppLayout.js
+++ client/components/AppLayout.js
@@ -1,9 +1,18 @@
 import PropTypes from "prop-types";
+import Link from "next/link";
 
 const AppLayout = ({ children }) => {
     return (
         <div>
-            <div>공통 메뉴</div>
+            <Link href="/">
+                <a>노드 버드</a>
+            </Link>
+            <Link href="/profile">
+                <a>프로필</a>
+            </Link>
+            <Link href="/signup">
+                <a>회원가입</a>
+            </Link>
             {children}
         </div>
     );
